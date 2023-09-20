94b122e89bc1d9c12b78b8175adc37f3bda17e7a
safo samson <safosamson18@gmail.com>
2023-09-20 12:42:27 +0100

    making tags options drop down

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -3,12 +3,12 @@ import IdeaList from './IdeaList';
 
 class IdeaForm {
   constructor() {
-    this._formModal = document.querySelector('#form-modal');
+    this._formModal = document.querySelector("#form-modal");
     this._ideaList = new IdeaList();
   }
 
   addEventListeners() {
-    this._form.addEventListener('submit', this.handleSubmit.bind(this));
+    this._form.addEventListener("submit", this.handleSubmit.bind(this));
   }
 
   async handleSubmit(e) {
@@ -19,12 +19,12 @@ class IdeaForm {
       !this._form.elements.tag.value ||
       !this._form.elements.username.value
     ) {
-      alert('Please enter all fields');
+      alert("Please enter all fields");
       return;
     }
 
     // Save user to local storage
-    localStorage.setItem('username', this._form.elements.username.value);
+    localStorage.setItem("username", this._form.elements.username.value);
 
     const idea = {
       text: this._form.elements.text.value,
@@ -39,36 +39,46 @@ class IdeaForm {
     this._ideaList.addIdeaToList(newIdea.data.data);
 
     // Clear fields
-    this._form.elements.text.value = '';
-    this._form.elements.tag.value = '';
-    this._form.elements.username.value = '';
+    this._form.elements.text.value = "";
+    this._form.elements.tag.value = "";
+    this._form.elements.username.value = "";
 
     this.render();
 
-    document.dispatchEvent(new Event('closemodal'));
+    document.dispatchEvent(new Event("closemodal"));
   }
 
   render() {
     this._formModal.innerHTML = `
-    <form id="idea-form">
-    <div class="form-control">
-      <label for="idea-text">Enter a Username</label>
-      <input type="text" name="username" id="username" value="${
-        localStorage.getItem('username') ? localStorage.getItem('username') : ''
-      }" />
-    </div>
-    <div class="form-control">
-      <label for="idea-text">What's Your Idea?</label>
-      <textarea name="text" id="idea-text"></textarea>
-    </div>
-    <div class="form-control">
-      <label for="tag">Tag</label>
-      <input type="text" name="tag" id="tag" />
-    </div>
-    <button class="btn" type="submit" id="submit">Submit</button>
-  </form>
+      <form id="idea-form">
+        <div class="form-control">
+          <label for="idea-text">Enter a Username</label>
+          <input type="text" name="username" id="username" value="${
+            localStorage.getItem("username")
+              ? localStorage.getItem("username")
+              : ""
+          }" />
+        </div>
+        <div class="form-control">
+          <label for="idea-text">What's Your Idea?</label>
+          <textarea name="text" id="idea-text"></textarea>
+        </div>
+        <div class="form-control">
+          <label for="tag">Tag</label>
+          <select name="tag" id="tag">
+            <option value="technology">Technology</option>
+            <option value="software">Software</option>
+            <option value="business">Business</option>
+            <option value="education">Education</option>
+            <option value="health">Health</option>
+            <option value="invention">Invention</option>
+            <option value="general">General</option>
+          </select>
+        </div>
+        <button class="btn" type="submit" id="submit">Submit</button>
+      </form>
     `;
-    this._form = document.querySelector('#idea-form');
+    this._form = document.querySelector("#idea-form");
     this.addEventListeners();
   }
 }
